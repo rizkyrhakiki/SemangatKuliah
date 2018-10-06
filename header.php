6edd8c9190fc73c65b60edd072dcb14abecd1f9c
rizkyrhakiki <rizkyrhakiki21@gmail.com>
2018-10-06 23:57:26 +0700

    navbar  header

--- header.php
+++ header.php
@@ -14,49 +14,4 @@
     <link rel="stylesheet" href="css/app.css">
     <title>Page Tittle</title>
 </head>
-
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand color-brand" href="">
-               <strong>NYELETUK</strong> 
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        <li>
-                            <div class="collapse navbar-collapse" id="">
-                                <form class="form-group has-search my-2 my-lg-0 ml-auto">
-                                
-                                <input class="form-control search-bar" type="search" placeholder="Cari Masalah..."
-                                        aria-label="Search">       
-                                    <span class="fa fa-search form-control-feedback"></span>                            
-                                    
-                                </form>
-                                
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href=""><strong>LOGIN</strong></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href=""><strong>REGISTER</strong></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-</body>
-
 </html>
