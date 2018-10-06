b16ee8a651c5224d558fc8e8d9bd1eacaaf4b24a
rizkyrhakiki <rizkyrhakiki21@gmail.com>
2018-10-06 23:54:33 +0700

    navbar nyeletuk

---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/bootsrap.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Page Tittle</title>
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+                <a class="navbar-brand color-brand" href="">
+               <strong>NYELETUK</strong> 
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        <li>
+                            <div class="collapse navbar-collapse" id="">
+                                <form class="form-group has-search my-2 my-lg-0 ml-auto">
+                                
+                                <input class="form-control search-bar" type="search" placeholder="Cari Masalah..."
+                                        aria-label="Search">       
+                                    <span class="fa fa-search form-control-feedback"></span>                            
+                                    
+                                </form>
+                                
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href=""><strong>LOGIN</strong></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href=""><strong>REGISTER</strong></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+</body>
+
+</html>
